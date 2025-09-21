efee59e821ab12862e4816636986ebf30188ceaf
Paulo Eduardo Lopes Bezerra <paulo_elb@hotmail.com>
2025-09-21 12:52:27 -0300

    Add project files.

---
+++ Exercicios.cs
@@ -0,0 +1,169 @@
+﻿namespace Aula_2_Console;
+
+public class Exercicios
+{
+    public double Media()
+    {
+        Console.WriteLine("*******Calculo de média*******");
+        Console.WriteLine("");
+
+        double nota1, nota2, nota3;
+        // Validação das notas
+
+        while (true)
+        {
+            Console.Write("Digite a nota 1 e tecle ENTER: ");
+            var notaInput = Console.ReadLine();
+            if (double.TryParse(notaInput, out nota1) && nota1 >= 0 && nota1 <= 10)
+                break;
+            Console.WriteLine("Erro: Digite uma nota válida (0-10)!");
+        }
+
+        while (true)
+        {
+            Console.Write("Digite a nota 2 e tecle ENTER: ");
+            var notaInput = Console.ReadLine();
+            if (double.TryParse(notaInput, out nota2) && nota2 >= 0 && nota2 <= 10)
+                break;
+            Console.WriteLine("Erro: Digite uma nota válida (0-10)!");
+        }
+
+        while (true)
+        {
+            Console.Write("Digite a nota 3 e tecle ENTER: ");
+            var notaInput = Console.ReadLine();
+            if (double.TryParse(notaInput, out nota3) && nota3 >= 0 && nota3 <= 10)
+                break;
+            Console.WriteLine("Erro: Digite uma nota válida (0-10)!");
+        }
+
+        // Cálculo da média
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        return media;
+    }
+
+    public int Somar()
+    {
+
+        int valor = 0;
+        int soma = 0;
+        do
+        {
+            Console.WriteLine("Somador de Números");
+            Console.WriteLine("");
+            Console.WriteLine("<<< Digite apenas números inteiros >>>");
+            Console.WriteLine("");
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Digite um número e tecle ENTER:");
+                    valor = int.Parse(Console.ReadLine());
+
+                    soma = soma + valor;
+
+                    Console.WriteLine("");
+                    Console.WriteLine($"O subtotal dos valores digitados é {soma}");
+                    Console.WriteLine("");
+                    Console.WriteLine("Pressione ENTER para continuar...");
+                    Console.ReadLine();
+                    Console.Clear();
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Erro: Digite um número válido!");
+                }
+            }
+        } while (valor != 0);
+
+        return soma;
+    }
+
+    public Dictionary<string, int> CadastroDeProdutos()
+    {
+
+        Console.WriteLine("****** Cadastro de produtos ******");
+        Console.WriteLine("");
+
+        Dictionary<string, int> estoque = new Dictionary<string, int>();
+        string produto;
+        int quantidade;
+
+        // Solicitar dados dos 3 produtos
+        for (int i = 1; i <= 3; i++)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Digite o nome do Produto {i} e tecle ENTER:");
+                produto = Console.ReadLine();
+                Console.WriteLine();
+
+                if (produto == null || produto.Trim().Length == 0 )
+                {
+                    Console.WriteLine("O nome do produto não pode ser vazio");
+                }
+                else 
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine($"Digite a quantidade em estoque para o Produto {i} e tecle ENTER: ");
+                string input = Console.ReadLine();
+                Console.WriteLine();
+
+                if (int.TryParse(input, out quantidade) && quantidade >= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Digite uma quantidade maior ou igual a zero");
+                }
+            }
+
+            estoque.Add(produto, quantidade);
+            Console.WriteLine("");
+        }
+
+        return estoque;
+    }
+
+    public double Divisao()
+    {
+        double divisao = 0.0;
+
+        try
+        {
+            Console.WriteLine("Digite o primeiro número e tecle ENTER: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número e tecle ENTER: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            //Tenta dividir dois números
+            divisao = num1 / num2;
+
+            //Não ocorrendo erro, faz a divisão com números double
+            divisao = (double)num1 / (double)num2;
+        }
+        catch (FormatException)
+        {
+            // Detectar entrada inválida
+            Console.WriteLine("Erro: Por favor, digite apenas números inteiros válidos.");
+        }
+        catch (DivideByZeroException)
+        {
+            // Tratar divisão por zero
+            Console.WriteLine("Erro: Não é possível dividir por zero.");
+        }
+
+        return divisao;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace Aula_2_Console;
+public class Program
+{
+    public static void Main()
+    {
+        Exercicios exercicios = new Exercicios();
+
+        while (true)
+        {
+            Console.WriteLine("Menu de aplicações");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Calcula média");
+            Console.WriteLine("2 - Soma números");
+            Console.WriteLine("3 - Cadastro de produtos");
+            Console.WriteLine("4 - Divide números");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("");
+            Console.WriteLine("Digite a opção desejada e tecle ENTER:");
+            var opcao = Console.ReadLine();
+
+            Console.Clear();
+
+            switch (opcao)
+            {
+                case "1":
+
+                    double media = exercicios.Media();
+
+                    Console.WriteLine("");
+                    Console.WriteLine($"A Média é {media:F2}");
+
+                    ExibeMensagemDeFinalizacao();
+                    break;
+
+                case "2":
+
+                    int soma = exercicios.Somar();
+
+                    Console.WriteLine("");
+                    Console.WriteLine($"A soma total dos valores digitados é {soma}");
+
+                    ExibeMensagemDeFinalizacao();
+                    break;
+
+                case "3":
+
+
+                    Dictionary<string, int> estoque = exercicios.CadastroDeProdutos();
+
+                    Console.Clear();
+                    Console.WriteLine("<<< Produtos cadastrados >>>");
+                    Console.WriteLine();
+
+                    foreach (var produto in estoque)
+                    {
+                        Console.WriteLine($"Produto: {produto.Key} – Quantidade: {produto.Value}");
+                    }
+
+                    ExibeMensagemDeFinalizacao();
+
+                    break;
+
+                case "4":
+
+                    double divisao = exercicios.Divisao();
+
+                    Console.WriteLine($"Resultado da divisão: {divisao:F2}");
+
+                    ExibeMensagemDeFinalizacao();
+
+                    break;
+
+                case "0":
+                    Console.WriteLine("");
+                    Console.WriteLine("Saindo...");
+                    Console.WriteLine("");
+                    return;
+
+                default:
+                    Console.WriteLine("");
+                    Console.WriteLine("Digite uma opção válida!");
+                    Console.WriteLine("Pressione ENTER para tentar novamente...");
+                    Console.ReadLine();
+                    Console.Clear();
+                    break;
+            }
+        }
+    }
+
+    public static void ExibeMensagemDeFinalizacao()
+    {
+        Console.WriteLine("");
+        Console.WriteLine("*******FIM*******");
+        Console.WriteLine("");
+        Console.WriteLine("Pressione ENTER para voltar ao menu principal...");
+        Console.ReadLine();
+        Console.Clear();
+    }
+}
+
